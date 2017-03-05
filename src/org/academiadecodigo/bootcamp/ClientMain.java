b75409c2d66915503022e7bfceb75129fa516cfe
Fred Akex <frederico.alexandre94@hotmail.com>
2017-03-05 04:16:00 +0000

    Change logic in Player Client relation and main

--- src/org/academiadecodigo/bootcamp/ClientMain.java
+++ src/org/academiadecodigo/bootcamp/ClientMain.java
@@ -7,11 +7,9 @@ import java.io.IOException;
  */
 public class ClientMain {
     public static void main(String[] args) {
-        try {
-            Client client = new Client();
-            client.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+        Player player = new Player();
+        player.play();
+
     }
 }
